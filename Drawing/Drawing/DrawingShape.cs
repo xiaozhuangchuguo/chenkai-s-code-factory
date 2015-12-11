a6503ca2375d22676a6f5796fa62ed426eaf51a0
chenkai <xiaozhuangchuguo@outlook.com>
2015-12-11 14:23:16 +0800

    Add first c# code

---
+++ Drawing/Drawing/DrawingShape.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace Drawing
+{
+    abstract class DrawingShape
+    {
+        protected int size;
+        protected int locX = 0, locY = 0;
+        protected Shape shape = null;
+
+        public DrawingShape(int size)
+        {
+            this.size = size;
+        }
+
+        public void SetLocation(int xCoord, int yCoord)
+        {
+            this.locX = xCoord;
+            this.locY = yCoord;
+        }
+
+        public void SetColor(Color color)
+        {
+            if (shape != null)
+            {
+                SolidColorBrush brush = new SolidColorBrush(color);
+                this.shape.Fill = brush;
+            }
+        }
+
+        public virtual void Draw(Canvas c)
+        {
+            if (this.shape == null)
+            {
+                throw new InvalidOperationException("Shape is Null");
+            }
+            else
+            {
+                this.shape.Height = this.size;
+                this.shape.Width = this.size;
+                Canvas.SetTop(this.shape, this.locY);
+                Canvas.SetLeft(this.shape, this.locX);
+                c.Children.Add(this.shape);
+            }
+        }
+    }
+}
