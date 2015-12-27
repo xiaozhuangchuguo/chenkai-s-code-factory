5515de989229325ff2568667e916a91593a735f6
chenkai <xiaozhuangchuguo@outlook.com>
2015-12-27 13:45:08 +0800

    对象初始化器与自动属性配合

---
+++ AutoProperties/AutoProperties/Polygon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProperties
+{
+    class Polygon
+    {
+        public int NumSides { get; set; }
+        public double Sidelength { get; set; }
+
+        public Polygon()
+        {
+            this.NumSides = 4;
+            this.Sidelength = 10.0;
+        }
+    }
+}
---
+++ AutoProperties/AutoProperties/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProperties
+{
+    class Program
+    {
+        static void doWork()
+        {
+            //
+            Polygon square = new Polygon();
+            Polygon triangle=new Polygon {NumSides=3};
+            Polygon pentagon = new Polygon { Sidelength = 15.5, NumSides = 5 };
+
+            Console.WriteLine("suqare:number of sides is {0},length of each side is {1}", square.NumSides, square.Sidelength);
+            Console.WriteLine("triangle:number of sides is {0},length of each side is {1}", triangle.NumSides, triangle.Sidelength);
+            Console.WriteLine("pentagon:number of sides is {0},length of each side is{ 1}", pentagon.NumSides, pentagon.Sidelength);
+
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
